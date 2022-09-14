3b7abb0af728a5bf01f847be2fcb8d3047933ca2
https://github.com/youngreal <dudwls0505@naver.com>
2022-09-14 16:57:52 +0900

    refactor: collecting Parameter 방식으로 refactoring

---
+++ xunit/TestSuite.java
@@ -0,0 +1,21 @@
+package xunit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestSuite {
+    List<TestCase> tests = new ArrayList<>();
+
+    public void add(TestCase test) {
+        tests.add(test);
+    }
+
+    /**
+     * 이전과 같이 테스트 하나씩 반환하는게 아니라 테스트를 누적시켜서 반환해야한다. 어떻게하지?
+     */
+    public void run(TestResult result) {
+        tests.forEach(t -> {
+            t.run(result);
+        });
+    }
+}

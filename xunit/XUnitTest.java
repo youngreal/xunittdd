1c9fc75485bc98dd505c70b6897952e96e2ee286
https://github.com/youngreal <dudwls0505@naver.com>
2022-09-14 17:00:30 +0900

    refactor: static factory method방식 적용

--- xunit/XUnitTest.java
+++ xunit/XUnitTest.java
@@ -2,12 +2,7 @@ package xunit;
 
 public class XUnitTest {
     public static void main(String[] args) {
-        TestSuite suite = new TestSuite();
-        suite.add(new TestCaseTest("testTemplateMethod"));
-        suite.add(new TestCaseTest("testResult"));
-        suite.add(new TestCaseTest("testFailedResultFormatting"));
-        suite.add(new TestCaseTest("testFailedResult"));
-        suite.add(new TestCaseTest("testSuite"));
+        TestSuite suite = TestCaseTest.suite();
         TestResult result = new TestResult();
         suite.run(result);
         System.out.println(result.getSummary());
